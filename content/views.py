13d6ea78a182c56efbf62c9a8e063f675548ca60
ByeongJunHan <appoung@naver.com>
2022-03-09 14:13:16 +0000

    20220309

--- content/views.py
+++ content/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from rest_framework.views import APIView
+from .models import Feed
 # Create your views here.
+class Main(APIView):
+    def get(self, request):
+        feed_list = Feed.objects.all()
+        print(feed_list)
+        return render(request, "junstagram/main.html")
